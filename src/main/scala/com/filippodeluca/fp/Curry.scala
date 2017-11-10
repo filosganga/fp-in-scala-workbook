8efe85017834b3e9647d4f1c138db513b859c7c6
Filippo De Luca <filippo.deluca@ovoenergy.com>
2017-11-10 13:36:53 +0000

    ex 2.5

--- src/main/scala/com/filippodeluca/fp/Curry.scala
+++ src/main/scala/com/filippodeluca/fp/Curry.scala
@@ -10,4 +10,6 @@ trait Curry {
     f(a)(b)
   }
 
+  def compose[A, B, C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
+
 }
--- src/test/scala/com/filippodeluca/fp/CurrySpec.scala
+++ src/test/scala/com/filippodeluca/fp/CurrySpec.scala
@@ -2,9 +2,11 @@ package com.filippodeluca.fp
 
 class CurrySpec extends UnitSpec with Curry {
 
+  def add(x: Int, y: Int) = x + y
+  def mul(x: Int, y: Int) = x * y
+
   "curry" should {
     "return a partially applied function" in {
-      def add(x: Int, y: Int) = x + y
 
       def add5 = curry(add)(5)
 
@@ -14,10 +16,15 @@ class CurrySpec extends UnitSpec with Curry {
 
   "uncurry" should {
     "return a uncurried function" in {
-      def add(x: Int, y: Int) = x + y
-
-
       uncurry(curry(add))(5, 3) shouldBe 5 + 3
+    }
+  }
+
+  "compose" should {
+    "apply the given functions in correct order" in {
+      val mul3AndThenAdd5 = compose(curry(add)(5), curry(mul)(3))
+
+      mul3AndThenAdd5(3) shouldBe (3 * 3 + 5)
     }
   }
 }
