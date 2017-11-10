785ba9343e9aee7299c83d2e0c5f57b7a7d56ca7
Filippo De Luca <filippo.deluca@ovoenergy.com>
2017-11-10 10:35:14 +0000

    Clean start

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ organization := "com.filippodeluca"
 
 name := "fp-in-scala"
 
-scalaVersion := "2.12.3"
+scalaVersion := "2.12.4"
 
 libraryDependencies ++= Seq (
   "org.scalatest" %% "scalatest" % "3.0.4" % Test,
--- src/test/scala/com/filippodeluca/fp/UnitSpec.scala
+++
@@ -1,9 +0,0 @@
-package com.filippodeluca.fp
-
-import org.scalatest.prop.PropertyChecks
-import org.scalatest.{Matchers, WordSpec}
-
-abstract class UnitSpec
-  extends WordSpec
-  with Matchers
-  with PropertyChecks
